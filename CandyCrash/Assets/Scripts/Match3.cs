bb299ab65ee3cb31b3c189325f7bec6516c6dc34
Ali Fuat <87227396+AliFuat98@users.noreply.github.com>
2025-07-21 15:35:09 +0300

    first grid system
    text mesh pro is added

---
+++ CandyCrash/Assets/Scripts/Match3.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Match3 : MonoBehaviour {
+    [SerializeField] int width = 8;
+    [SerializeField] int height = 8;
+    [SerializeField] float cellSize = 1f;
+    [SerializeField] Vector3 originPosition = Vector3.zero;
+    [SerializeField] bool debug = true;
+
+    GridSystem2D<GridObject<Gem>> grid;
+
+    void Start() {
+        grid = GridSystem2D<GridObject<Gem>>.VerticalGrid(width, height, cellSize, originPosition, debug);
+    }
+}
